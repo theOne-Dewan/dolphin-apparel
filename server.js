7fa44cc54c2f5bd211152fa4b7148424607f9685
TheOne <dewan_sayeed@outlook.com>
2021-12-08 23:25:01 -0600

    Added Service Worker, HTTPS and updated manifest JSON to make the WebApp more PWA compliant

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -13,14 +14,20 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 
 if(process.env.NODE_ENV === 'production'){
+    app.use(enforce.HTTPS({trustProtoHeader: true}));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req, res){
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 };
+
+app.get('/service-worker.js', (req,res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 app.listen(port, error => {
     if(error) throw error;
