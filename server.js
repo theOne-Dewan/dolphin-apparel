e7676b03f3d3c69a69d3ddcf2beba03261a3a03a
TheOne <dewan_sayeed@outlook.com>
2021-12-09 05:29:03 -0600

    Fixed the favicon size issue in manifest by resizing the actual ico file. Changed the path in server.js for service-worker

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'production'){
 };
 
 app.get('/service-worker.js', (req,res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'service-worker.js'));
 });
 
 app.listen(port, error => {
